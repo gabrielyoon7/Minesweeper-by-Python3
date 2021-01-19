d787ecf9b795df0e0062ae717db680c23c8317af
user <gabrielyoon7@gmail.com>
2021-01-19 21:48:59 +0900

    커밋 메시지 작성

--- main.py
+++ main.py
@@ -10,11 +10,14 @@ def print_hi(name):
 
 
 # Press the green button in the gutter to run the script.
+# 아랫줄(if __name__...)의 의미를 자세히 알고싶다면 https://medium.com/@chullino/if-name-main-%EC%9D%80-%EC%99%9C-%ED%95%84%EC%9A%94%ED%95%A0%EA%B9%8C-bc48cba7f720 참조.
+# (요약 : 이 main.py에서 실행되는 경우에만 코드를 실행하겠다는 의미임. 다른 py파일이 이 main.py를 import 하는 경우에는 이름이 다르기 때문에 실행하지 않음.)
 if __name__ == '__main__':
     print_hi('PyCharm')
     print("This is first push.")
     print("두번째 푸시 테스트")
     print("세번째 푸시 테스트")
     print("네번째 푸시 테스트")
+    print("555555")
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
