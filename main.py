2bd49ee5fec8255efc1a420746a3ebfc3f763a1f
user <gabrielyoon7@gmail.com>
2021-01-20 20:48:58 +0900

    settings 변수 main에서 사용할 수 있게 변경

--- main.py
+++ main.py
@@ -2,25 +2,26 @@
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-import sys, pygame, settings
+import sys, pygame
+from settings import *
 from pygame.locals import *
 
 # Press the green button in the gutter to run the script.
 class Tetris:
     def __init__(self):
-        self.width = 16*settings.BWIDTH  #플레이 영역 가로줄 길이 = 블럭 16개 길이를 의미
-        self.height = 30*settings.BHEIGHT  #플레이 영역 세로줄 길이 = 블럭 30개 길이를 의미
+        self.width = 16*BWIDTH  #플레이 영역 가로줄 길이 = 블럭 16개 길이를 의미
+        self.height = 30*BHEIGHT  #플레이 영역 세로줄 길이 = 블럭 30개 길이를 의미
 
         # Rect(x좌표, y좌표, 가로길이, 세로길이) 단, 가로길이와 세로길이 둘 중 하나는 두께 값임.
         # 경계선 한줄 한줄을 사각형으로 그림 (그려진 사각형 배치는 drawPlayBorder에서 함)
-        self.borderUp = pygame.Rect(settings.X, settings.Y, self.width, settings.THICKNESS)
-        self.borderLeft = pygame.Rect(settings.X, settings.Y, settings.THICKNESS, self.height)
-        self.borderDown = pygame.Rect(settings.X, settings.Y + self.height, self.width, settings.THICKNESS)
-        self.borderRight = pygame.Rect(settings.X + self.width - settings.THICKNESS, settings.Y, settings.THICKNESS, self.height)
+        self.borderUp = pygame.Rect(X, Y, self.width, THICKNESS)
+        self.borderLeft = pygame.Rect(X, Y, THICKNESS, self.height)
+        self.borderDown = pygame.Rect(X, Y + self.height, self.width, THICKNESS)
+        self.borderRight = pygame.Rect(X + self.width - THICKNESS, Y, THICKNESS, self.height)
 
     def run(self):
         pygame.init() #pygame 초기화. 초기화를 해야 pygame을 사용할 수 있다고 함.
-        self.screen = pygame.display.set_mode(settings.SCREEN_SIZE) #디스플레이 크기 설정
+        self.screen = pygame.display.set_mode(SCREEN_SIZE) #디스플레이 크기 설정
         pygame.display.set_caption('Tetris') #프로그램 이름 설정
         while True:  # main game loop (게임에 필요한 메소드 불러오기)
             #조작 방법 실행 (미구현)
@@ -36,15 +37,15 @@ class Tetris:
             pygame.display.update()
 
     def showGame(self): #그래픽 실행.
-        self.screen.fill(settings.WHITE)
+        self.screen.fill(WHITE)
         self.drawPlayBorder()
         self.drawScoreBoard()
 
     def drawPlayBorder(self): #블럭이 움직일 수 있는 경계선 그리기
-        pygame.draw.rect(self.screen,settings.BLACK,self.borderUp)
-        pygame.draw.rect(self.screen,settings.BLACK,self.borderDown)
-        pygame.draw.rect(self.screen,settings.BLACK,self.borderLeft)
-        pygame.draw.rect(self.screen,settings.BLACK,self.borderRight)
+        pygame.draw.rect(self.screen,BLACK,self.borderUp)
+        pygame.draw.rect(self.screen,BLACK,self.borderDown)
+        pygame.draw.rect(self.screen,BLACK,self.borderLeft)
+        pygame.draw.rect(self.screen,BLACK,self.borderRight)
 
     def drawScoreBoard(self): #현재 점수 표시하는 영역(미완성)
         pass
