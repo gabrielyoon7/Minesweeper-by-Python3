39553ddb0964dc31d3ce9dfa5665e73ea9137b4b
gabrielyoon7 <gabrielyoon7@gmail.com>
2021-01-27 20:06:03 +0900

    [ADD] 버튼 누르면 그 버튼 위에 텍스트가 출력되게 함. (지뢰는 표현 못함. 이유는 찾아야함.)

--- gui.py
+++ gui.py
@@ -11,12 +11,15 @@ class GUI(): #임시. pygame 안써도 됨.
     def __init__(self, level): #초기화
         pygame.init()  # pygame 초기화. 초기화를 해야 pygame을 사용할 수 있다고 함.
         super().__init__()
+
         self.count = 0
         self.screen = pygame.display.set_mode(SCREEN_SIZE)  # 디스플레이 크기 설정
         pygame.display.set_caption('Minesweeper')  # 프로그램 이름 설정
         gameLevel=self.getLevel(level) #레벨을 tuple 형태로 받아옴.
         arr = GameLogic.createMap(self, gameLevel) #맵을 생성하고 저장
         OPENED = [[False for row in range(len(arr))] for column in range(len(arr[0]))]
+        self.draw_Cells(arr)  # 칸 그리기
+
         while True:  # main game loop (게임에 필요한 메소드 불러오기)
             for event in pygame.event.get(): #창 안의 이벤트를 받는 영역. 예를 들면 종료키, 키보드키, 마우스 클릭 등
                 if event.type == QUIT:#상단의 X키 누를 때 까지 프로그램 종료 안하고 유지하기 (필수임)
@@ -30,10 +33,8 @@ class GUI(): #임시. pygame 안써도 됨.
                         if arr[column_index][row_index] == 'X':  # 선택된 칸이 X이면 종료, (오류)선택해도 자꾸 open_Cell 함수로 넘어감
                             print("패배")
                         else:  # 선택된 칸 오픈
-                            print("1")
                             arr=self.open_Cell(arr,OPENED, column_index, row_index)
-                            print("2")
-                self.draw_Cells(arr)  # 칸 그리기
+#                self.draw_Cells(arr)  # 칸 그리기
             pygame.display.update()
 
     def getLevel(self, level): #레벨 가져오기(나중에 수정 필요)
@@ -56,6 +57,7 @@ class GUI(): #임시. pygame 안써도 됨.
                 pygame.draw.rect(self.screen, BLACK, rect, 1)
 
     def open_Cell(self,arr,OPENED,col,row):
+
         print("open_Cell is running1")
         if col < 0 or col >= len(arr) or row < 0 or row >= len(arr[0]):
             print("open_Cell is running2")
@@ -83,4 +85,11 @@ class GUI(): #임시. pygame 안써도 됨.
             self.open_Cell(arr, OPENED,col - 1, row - 1)
             self.open_Cell(arr, OPENED,col + 1, row - 1)
             self.open_Cell(arr, OPENED,col - 1, row + 1)
+        print("open_Cell is running10")
+        font5 = pygame.font.SysFont('notosanscjkkrblack', 50)
+        print("open_Cell is running11")
+        img5 = font5.render(str(cell), True, RED)
+        print("open_Cell is running12")
+        self.screen.blit(img5, (50*col+0, 50*row+0))
+        print("open_Cell is running13")
         return arr
